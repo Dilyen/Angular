ba304f3cb8980988b3b726bd223276d60c70b859
Dilyen <davidandohacquah@gmail.com>
2019-12-05 13:50:26 +0000

    injected routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './Components/main/main.component';
+import { NewclientComponent } from './Components/newclient/newclient.component';
+import { NewprojectComponent } from './Components/newproject/newproject.component';
+import { ProjectDetailsComponent } from './Components/project-details/project-details.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
+import { ReportingComponent } from './Components/reporting/reporting.component';
+import { ClientsComponent } from './Components/clients/clients.component';
+import { ClientDetailsComponent } from './Components/client-details/client-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'main', component:MainComponent},
+  {path:'newclient', component:NewclientComponent},
+  {path:'newproject', component:NewprojectComponent},
+  {path:'project-details', component:ProjectDetailsComponent},
+  {path:'projects', component:ProjectsComponent},
+  {path:'reporting', component:ReportingComponent},
+  {path:'clients', component:ClientsComponent},
+  {path:'client-details', component:ClientDetailsComponent}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
